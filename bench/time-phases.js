c4c91b84e91f83ea2250b889d5f053cc79b8787e
Joe Gibbs Politz <joe.politz@gmail.com>
2016-12-23 22:38:22 -0800

    skip measurements if an error occurs

--- bench/time-phases.js
+++ bench/time-phases.js
@@ -2,44 +2,48 @@ define(["child_process"], function(childProcess) {
 
   function time(f) {
     var start = process.hrtime();
-    var ans = f();
-    return [ans, process.hrtime(start)];
+    try {
+      var ans = f();
+      return [true, ans, process.hrtime(start)];
+    }
+    catch(e) {
+      return [false, String(e), process.hrtime(start)]
+    }
   }
 
   function hrtimeToMicroseconds(t) {
     return (t[0] * 1000000) + (t[1] / 1000);
   }
 
-
   function run(makeMeasurements, options) {
 
     const workDir = options.workDir;
     
     childProcess.execSync("npm install", {cwd: workDir});
-    const [, timeA] = time(() =>
-      childProcess.execSync("make phaseA", {cwd: workDir}));
-    const [, timeB] = time(() =>
-      childProcess.execSync("make phaseB", {cwd: workDir}));
-    const [, timeC] = time(() =>
-      childProcess.execSync("make phaseC", {cwd: workDir}));
+    const [aSuccess, , timeA] = time(() =>
+      childProcess.execSync("echo 'make phaseA'", {cwd: workDir}));
+    const [bSuccess, , timeB] = time(() =>
+      childProcess.execSync("cat non-existst.txt", {cwd: workDir}));
+    const [cSuccess, timeC] = time(() =>
+      childProcess.execSync("echo 'make phaseC'", {cwd: workDir}));
 
     return makeMeasurements([
-      {
+      aSuccess && {
         labels: ["js-wall-time", "compiler-build", "phaseA"],
         measurement: hrtimeToMicroseconds(timeA),
         unit: "microseconds"
       },
-      {
+      bSuccess && {
         labels: ["js-wall-time", "compiler-build", "phaseB"],
         measurement: hrtimeToMicroseconds(timeB),
         unit: "microseconds"
       },
-      {
+      cSuccess && {
         labels: ["js-wall-time", "compiler-build", "phaseC"],
         measurement: hrtimeToMicroseconds(timeC),
         unit: "microseconds"
       }
-    ]);
+    ].filter((a) => a));
   }
 
   return {
